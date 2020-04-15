671ad2d674e2d55b34f1aeccfa8cde6075bc3f16
Nir Ben-Zvi <nzvi@trigo.tech>
2020-04-15 16:56:00 +0300

    Made my code simialr to Ira's and fixed requirements. Both classes are now superclasses of an abstrat class for spotify authentication.

---
+++ spotify_abs_cls.py
@@ -0,0 +1,16 @@
+import utils
+
+
+class SpotifuHandler(object):
+    """
+    Superclass for spotify fun
+    """
+    def __init__(self, spotify_yaml_path):
+        """
+        see the parse_args function for documentation on the parameters.
+        """
+        self._spotify_yaml_path = spotify_yaml_path
+        config = utils.read_yaml(self._spotify_yaml_path)
+        self._user_id = config["user_id"]
+        self._token = config["OAuth_Token"]
+        self._sp = utils.get_spotipy_object(self._token)
